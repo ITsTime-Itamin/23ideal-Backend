cc185e1820f078d2b5c5899f458f801d7177d338
kimjungwoo <rlawjddn5980@naver.com>
2022-03-21 23:23:29 +0900

    init commit

---
+++ src/main/java/com/example/itaminbackend/ItaminBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.itaminbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaminBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItaminBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itaminbackend/ItaminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itaminbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class ItaminBackendApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
