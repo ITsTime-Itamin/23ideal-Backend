836b69108770847daebc2270893e61c99ca37722
kimjungwoo <rlawjddn5980@naver.com>
2022-03-21 23:23:29 +0900

    init commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(sudo docker container ls -q)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+else
+  echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
+  sudo docker stop $CURRENT_PID
+  sleep 5
+fi
+
+cd /home/ec2-user/itamin/zip
+
+sudo docker-compose down
+
+sudo docker rmi itamin-backend
+
+sudo docker build -t itamin-backend .
+
+sudo docker-compose up
