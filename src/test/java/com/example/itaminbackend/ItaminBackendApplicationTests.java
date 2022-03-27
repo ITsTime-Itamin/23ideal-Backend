b443a020965ab50d74f8da39d054b7203a0f385e
Minsu <syt07100@gmail.com>
2022-03-27 17:16:24 +0900

    test

--- src/test/java/com/example/itaminbackend/ItaminBackendApplicationTests.java
+++ src/test/java/com/example/itaminbackend/ItaminBackendApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.itaminbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class ItaminBackendApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+@SpringBootTest
+class ItaminBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
