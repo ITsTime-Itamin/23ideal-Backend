58084eb4fb43da9593f39b6dd1d6a7c2140033e8
kimjungwoo <rlawjddn5980@naver.com>
2022-03-29 12:27:51 +0900

    chore(II-38): Swagger environment configuration

---
+++ src/main/java/com/example/itaminbackend/domain/house/controller/HouseController.java
@@ -0,0 +1,14 @@
+package com.example.itaminbackend.domain.house.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("api/v1/houses")
+//@Api(tags = "House API")
+//public class HouseController {
+//
+//
+//}
