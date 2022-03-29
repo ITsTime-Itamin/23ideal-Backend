b01f4d4be630377fbc9cea413118fe636e662b45
kimjungwoo <rlawjddn5980@naver.com>
2022-03-29 12:27:51 +0900

    chore(II-38): Swagger environment configuration

---
+++ src/main/java/com/example/itaminbackend/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.example.itaminbackend.global.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        String description = "Welcome to Itamin!";
+        return new ApiInfoBuilder()
+                .title("Itamin API Documentation")
+                .description(description)
+                .version("1.0")
+                .build();
+    }
+}
