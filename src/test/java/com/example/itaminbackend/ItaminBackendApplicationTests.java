78f561deccf615ac3a3d9aea25125c62396d2c0d
friendshipkim97 <rlawjddn5980@naver.com>
2022-04-05 12:49:18 +0900

    chore(II-37): fix profiles

--- src/test/java/com/example/itaminbackend/ItaminBackendApplicationTests.java
+++ src/test/java/com/example/itaminbackend/ItaminBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.itaminbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile("test")
 class ItaminBackendApplicationTests {
 
     @Test
