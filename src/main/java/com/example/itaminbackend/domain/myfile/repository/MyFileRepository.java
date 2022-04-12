aa013349ca07efd142d0a2587da9e72e1938ad29
friendshipkim97 <rlawjddn5980@naver.com>
2022-04-12 13:22:08 +0900

    feat(II-42, II-44, II-45): add myFile services

--- src/main/java/com/example/itaminbackend/domain/myfile/repository/MyFileRepository.java
+++ src/main/java/com/example/itaminbackend/domain/myfile/repository/MyFileRepository.java
@@ -3,5 +3,6 @@ package com.example.itaminbackend.domain.myfile.repository;
 import com.example.itaminbackend.domain.myfile.entity.MyFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MyFileRepository extends JpaRepository<MyFile, Long> {
+public interface MyFileRepository extends JpaRepository<MyFile, Long>, MyFileRepositoryCustom {
+
 }
